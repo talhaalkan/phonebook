0369ae12e5790ee0ccb3b9baa87b82cf1340ebe7
Talha Alkan <alkantalha10@gmail.com>
2023-01-17 18:16:55 +0300

    updated

--- user-data.sh
+++ user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="..."
+TOKEN="..."  # using parameter store is best practice
 cd /home/ec2-user && git clone https://$TOKEN@github.com/talhaalkan/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook/phonebook-app.py
